15669d350873c2df2b0d1a74d9f6e1cdfee6907c
farghl2 <islammohammed2001@gmail.com>
2023-05-15 19:03:56 +0300

    first commit

---
+++ lib/my_contact.dart
@@ -0,0 +1,110 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart';
+import 'package:my_contacts/wedgate/social_media_icons.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
+// ignore: must_be_immutable
+class MyContact extends StatelessWidget {
+  MyContact({super.key});
+   Map<String, String>socialMedia ={
+    'facebook.png':'https://www.facebook.com/islam.farghl',
+    'github.png':'https://github.com/farghl2',
+    'instagram.png':'https://www.instagram.com/islammohammed11/',
+    'twitter.png':'https://twitter.com/?lang=en',
+    'youtube.png':'https://www.youtube.com/',
+    'whatsapp.jpg':'https://wa.me/+201122882154'
+    } ;
+  
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 3, 7, 30),
+      body: SizedBox(
+        width: double.infinity,
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            // backgroundColor:Colors.red,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 100,
+              ),
+              const CircleAvatar(
+                radius: 100,
+                backgroundImage: AssetImage('assets/islam.jpg'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                'Islam Mohammed',
+                style: TextStyle(
+                  fontSize: 19,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+          
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text(
+                      '+201122882154',
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    IconButton(
+              icon: const Icon(
+          Icons.phone,
+          size: 30,
+          color: Colors.grey,
+              ),
+              onPressed: (() {
+                launchUrl(Uri.parse('tel:+201122882154'));
+              }),
+          )
+                  ]),
+                  const SizedBox(
+                    height: 20,
+                  ),
+          
+                  GridView.builder(
+                    itemCount: socialMedia.length,
+                    gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3)
+                     , itemBuilder: (context, index) {
+              
+                        return  SocialMediaIcon(socialMediaImg: socialMedia.keys.toList()[index], socialMediaUrl:socialMedia.values.toList()[index] ,);
+                     },
+                     shrinkWrap: true,
+                     physics: const NeverScrollableScrollPhysics(),
+                     padding: const EdgeInsets.all(20),
+                     
+                     )
+                //  const SocialMediaIcon(socialMedia: 'facebook.png'),
+                
+            ],
+          ),
+        ),
+      ),
+      
+      
+    );
+  }
+}
+
+
---
+++ lib/wedgate/social_media_icons.dart
@@ -0,0 +1,44 @@
+
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
+
+
+
+class SocialMediaIcon extends StatelessWidget {
+  final String socialMediaImg; 
+  final String socialMediaUrl;
+  
+  const SocialMediaIcon({
+    Key? key,
+     required this.socialMediaImg,
+     required this.socialMediaUrl
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: InkWell(
+            child: CircleAvatar(
+                  backgroundColor: Colors.transparent,
+                  radius: 30,
+                  backgroundImage: AssetImage('assets/$socialMediaImg'),
+                
+                ),
+                onTap:() {
+                  launchUrl(Uri.parse(socialMediaUrl), mode: LaunchMode.externalApplication);
+
+                }
+
+          ),
+          
+            
+          
+        );
+       
+    
+  }
+}
